bbef8642a4d0de25a3cca93b8643ea471999ac44
Chalo1996 <emusyoka759@gmail.com>
2022-06-27 13:28:55 +0000

    Slicing a word severally

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-str1 = word[:4]
-str2 = word[-2:]
-str3 = word[1:-1]
-print(f"First 3 letters: {str1}")
-print(f"Last 2 letters: {str2}")
-print(f"Middle word: {str3}")
+word_first_3 = word[:4]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
