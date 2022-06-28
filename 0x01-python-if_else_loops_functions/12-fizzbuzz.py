1372941529b26930b4519d0093b87d9b16f68d1e
Chalo1996 <emusyoka759@gmail.com>
2022-06-28 21:44:04 +0000

    The FizzBuzz Challange

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,9 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        elif i % 15 == 0:
-            print("FizzBuzz", end=" ")
-        else:
-            print(i, end=" ")
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 5 and i % 3:
+            print("{:d}".format(i), end="")
+        print(end = " ")
