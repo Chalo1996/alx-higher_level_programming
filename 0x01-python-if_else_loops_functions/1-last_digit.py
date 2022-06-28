a929120f8b4f4ee78249bf7806d6106752f0052d
Chalo1996 <emusyoka759@gmail.com>
2022-06-28 16:10:02 +0000

    Get last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000) 
+number = random.randint(-10000, 10000)
 mystr = "Last digit of"
 if number > 0:
     last_digit = number % 10
