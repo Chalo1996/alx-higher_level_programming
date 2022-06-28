b49c48a9ebcb77025585f98d32d4c0abdcce13c0
Chalo1996 <emusyoka759@gmail.com>
2022-06-28 16:24:18 +0000

    Last digit checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,9 @@ if number > 0:
         print(f"{mystr} {number} is {last_digit} and is greater than 5")
     elif last_digit == 0:
         print(f"{mystr} {number} is {last_digit} and is 0")
-    elif last_digit < 6 and last_digit != 0:
+    else:
         print(f"{mystr} {number} is {last_digit} and is less than 6 and not 0")
 else:
     last_digit = number % -10
-    print(f"{mystr} {number} is {last_digit} and is less than 6 and not 0")
+    if last_digit != 0:
+        print(f"{mystr} {number} is {last_digit} and is less than 6 and not 0")
