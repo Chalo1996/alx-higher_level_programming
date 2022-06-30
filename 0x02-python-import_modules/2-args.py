1e5b04d3329834a4665b26c71c2d260ea224219a
Chalo1996 <emusyoka759@gmail.com>
2022-06-30 14:17:00 +0000

    commandline args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     index = 1
     myArgs = sys.argv
     len_myArgs = len(myArgs)
-    print("{:d} arguments:".format(len_myArgs - 1))
+    print("{:d} {:s}{:s}".format(len_myArgs - 1, "argument" if len_myArgs <= 2 else "arguments", "." if len_myArgs == 1 else ":"))
     for arg in myArgs:
         if arg != myArgs[0]:
             print("{:d}: {}".format(index, arg))
