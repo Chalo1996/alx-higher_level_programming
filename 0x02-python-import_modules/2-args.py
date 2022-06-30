980f6705e46c36baf0d279f025ad8f33744b4ac4
Chalo1996 <emusyoka759@gmail.com>
2022-06-30 14:56:26 +0000

    Commandline arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,13 @@ if __name__ == "__main__":
     import sys
     index = 1
     myArgs = sys.argv
-    len_myArgs = len(myArgs)
-    print("{:d} {:s}{:s}".format(len_myArgs - 1, "argument" if len_myArgs <= 2 else "arguments", "." if len_myArgs == 1 else ":"))
+    len_myArgs = len(myArgs) - 1
+    if len_myArgs == 1:
+        print("{:d} argument:".format(len_myArgs))
+    elif len_myArgs >= 2:
+        print("{:d} arguments:".format(len_myArgs))
+    else:
+        print("{:d} arguments.".format(len_myArgs))
     for arg in myArgs:
         if arg != myArgs[0]:
             print("{:d}: {}".format(index, arg))
