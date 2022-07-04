145fdab8d56c3f82e05912b997c04dd13a3bb43f
Chalo1996 <emusyoka759@gmail.com>
2022-07-04 18:23:23 +0000

    Retrieves an element at an index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lenElements = len(my_list)
-    if idx < 0 or idx > lenElements:
+    if idx < 0 or idx >= lenElements:
         return None
     return idx + 1
