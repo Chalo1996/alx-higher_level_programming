c23a14005ee0b2d62754377c545aa7fe28157409
Chalo1996 <emusyoka759@gmail.com>
2022-07-04 21:50:28 +0000

    Return length of a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return "None"
+    if sentence[0] == None:
+        return None
     return (len(sentence), sentence[0])
