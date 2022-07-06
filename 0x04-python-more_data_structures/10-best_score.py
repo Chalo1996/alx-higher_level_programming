83874efa5c8981ce55cc7e305e30a40471a64a93
Chalo1996 <emusyoka759@gmail.com>
2022-07-06 21:14:24 +0000

    Return Best Max Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    maxKey = 0
+    if not a_dictionary:
+        return None
+    else:
+        for k in a_dictionary:
+            if a_dictionary[k] > maxKey:
+                maxKey = a_dictionary[k]
+        val_list = list(a_dictionary.values())
+        key_list = list(a_dictionary.keys())
+        val = val_list.index(maxKey)
+        return key_list[val]
