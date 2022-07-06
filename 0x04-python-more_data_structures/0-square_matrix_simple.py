7a1202aa6b69e80f4786ab3ce64b4ef909b0789f
Chalo1996 <emusyoka759@gmail.com>
2022-07-06 15:13:25 +0000

    removed irrelevant line

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,4 @@ def square_matrix_simple(matrix=[]):
     def sq(num):
         return num * num
     if matrix:
-        myIter = [j for j in matrix]
-        return [list(map(sq, i)) for i in myIter]
+        return [list(map(sq, i)) for i in matrix]
