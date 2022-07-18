1b7d86b162a0345db1037e353c6f95de3d38f9e5
Chalo1996 <emusyoka759@gmail.com>
2022-07-18 19:22:23 +0000

    Execute a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+
+    except Exception as err:
+        import sys
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
