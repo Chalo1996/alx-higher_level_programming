dae81e893c3350a7c5d6e04fdf4a5baf67682c08
Chalo1996 <emusyoka759@gmail.com>
2022-07-19 18:58:28 +0000

    Instantiation with optional

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Creates an class object"""
+
+
+class Square:
+    """Defines a Square"""
+    def __init__(self, size=0):
+        """Creates a private instance attribute
+
+           properties of the attribute:
+
+           size must be an integer othewise raise
+           exception TypeError and must be >= 0
+           otherwise raise exception ValueError.
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
