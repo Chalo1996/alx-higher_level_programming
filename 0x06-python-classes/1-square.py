11e3de9e4ccc5bfd93774f3e80f75d9cb7b0b429
Chalo1996 <emusyoka759@gmail.com>
2022-07-19 18:25:53 +0000

    Privete instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates an class object"""
+
+
+class Square:
+    """Defines a Square"""
+    def __init__(self, size):
+        """Creates a private instance attribute"""
+        self.__size = size
