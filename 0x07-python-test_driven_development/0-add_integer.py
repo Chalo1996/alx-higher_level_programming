7d3af8aaa855d69ed043c3466e9c6e184daf8070
Chalo1996 <emusyoka759@gmail.com>
2022-07-26 20:39:53 +0000

    Corrected exception raise

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,8 +18,9 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
 
-    if type(a) is not int or type(b) is not int:
+    if type(a) is not int:
         raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
 
-    else:
-        return "{:d}".format(a + b)
+    return a + b
