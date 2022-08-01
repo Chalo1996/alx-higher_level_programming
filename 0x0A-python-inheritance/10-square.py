0415003b93243e7b7d88ee63e073f034785c2e59
Chalo1996 <emusyoka759@gmail.com>
2022-08-01 22:11:52 +0000

    Implements a square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Contains the Square class
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle
+    """
+    def __init__(self, size):
+        """Instantiates size
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Implements string format
+        """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """implements area
+
+        Return:
+            The return. Neatly formated area
+        """
+        res = self.__size * self.__size
+        return res
