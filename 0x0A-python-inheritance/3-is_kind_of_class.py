35272baf63823fb888de9d70fa8fe59b173e3fa8
Chalo1996 <emusyoka759@gmail.com>
2022-08-01 17:36:04 +0000

    Returns isinstance object of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns isinstance object of a_class
+    """
+
+    return isinstance(obj, a_class)
