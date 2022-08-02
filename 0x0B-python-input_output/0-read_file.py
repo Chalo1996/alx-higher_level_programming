baddffedce359235cc8e292c947f695c2f36bdef
Chalo1996 <emusyoka759@gmail.com>
2022-08-02 19:33:33 +0000

    reads file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Contains the read_file function
+"""
+
+
+def read_file(filename="my_file_0.txt"):
+    """Reads a text file
+    """
+    with open(filename, encoding="utf-8") as f:
+        readfile = f.read()
+        print(readfile)
