6b895c0fb8b4995024a51ed6bff26a3db3146af8
Chalo1996 <emusyoka759@gmail.com>
2022-08-02 19:38:01 +0000

    reads file content

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """Reads a text file
     """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         readfile = f.read()
         print(readfile)
