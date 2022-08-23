4a2df992f81488a3cbdaf777f2ea0b43924c2f70
Chalo1996 <emusyoka759@gmail.com>
2022-08-23 16:40:11 +0000

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -45,11 +45,14 @@ class Square(Rectangle):
 
     def __str__(self):
         return "[Square] \
-            ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.size)
+            ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width)
 
     def to_dictionary(self):
         """returns a dictionary representation of a square
         """
-        return {
-            'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y
-        }
+        dict = {}
+        dict["id"] = self.id
+        dict["size"] = self.size
+        dict["x"] = self.x
+        dict["y"] = self.y
+        return dict
