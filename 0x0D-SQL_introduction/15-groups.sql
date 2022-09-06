b1d1a2cb84db808007f66660ee8c9ae0d56b4792
Chalo1996 <emusyoka759@gmail.com>
2022-09-06 22:53:43 +0000

    count

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,10 @@
+-- ists the number of records with the same score in the table second_table
+-- The result should display:
+--    the score
+--    the number of records for this score   with the label number
+-- The list should be sorted by the number of records (descending)
+
+SELECT score, COUNT(*) as number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
