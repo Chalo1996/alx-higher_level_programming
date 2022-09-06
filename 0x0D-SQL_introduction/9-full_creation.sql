9857bbb6f0eb59cd8bb5167e6a62a4a20659b0e1
Chalo1996 <emusyoka759@gmail.com>
2022-09-06 22:25:29 +0000

    create a table and insert values into it

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,23 @@
+-- creates a table second_table in the database hbtn_0c_0
+-- and add multiples rows
+-- second_table description:
+--      id INT
+--      name VARCHAR(256)
+--      score INT
+-- The database name will be passed as an argument to the mysql command
+-- If the table second_table already exists, your script should not fail
+-- You are not allowed to use the SELECT and SHOW statements
+-- Your script should create these records: 
+
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
