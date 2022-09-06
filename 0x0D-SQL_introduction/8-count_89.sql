780c9c2a63d82796f579d2274feb50a0e4de64ca
Chalo1996 <emusyoka759@gmail.com>
2022-09-06 22:05:42 +0000

    count number of specific values

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- displays the number of records with id = 89 in the table first_table
+-- The database name will be passed as an argument of the mysql command
+
+SELECT COUNT(id) FROM first_table WHERE id = 89;
