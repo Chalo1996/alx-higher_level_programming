a4a68759b13fabb01b4fd8a951173de0140761cd
Chalo1996 <emusyoka759@gmail.com>
2022-09-06 22:40:24 +0000

    delete value from a table

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+-- removes all records with a score <= 5 in the table second_table
+-- The database name will be passed as an argument of the mysql command
+
+DELETE FROM second_table
+WHERE score <= 5;
