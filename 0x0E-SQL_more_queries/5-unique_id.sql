5e41de337bc789466326030f2a03d099c5ae9225
Chalo1996 <emusyoka759@gmail.com>
2022-09-07 22:44:06 +0000

    unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -6,6 +6,6 @@
 -- If the table unique_id already exists, your script should not fail
 
 CREATE TABLE IF NOT EXISTS unique_id(
-    id INT UNIQUE DAFAULT 1,
+    id INT UNIQUE DEFAULT 1,
     name VARCHAR
 );
