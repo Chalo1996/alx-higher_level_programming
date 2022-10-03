da56a709fdb48e546e9eb8b13a46518c5306fa5d
Chalo1996 <emusyoka759@gmail.com>
2022-10-04 02:09:41 +0300

    second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,35 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let biggest = process.argv[2];
-const firstArg = process.argv[0];
-const secondArg = process.argv[1];
-let i;
+// const args = process.argv;
+// let biggest = process.argv[2];
+// const firstArg = process.argv[0];
+// const secondArg = process.argv[1];
+// let i;
 
-if (args.length > 1 && args !== firstArg && args !== secondArg) {
-  for (let i = 0; i < args.length; i++) {
-    if (biggest < process.argv[i]) {
-      biggest = process.argv[i];
-    }
-  }
-  let secondBiggest = process.argv[3];
-  for (let j = 0; j < args.length; j++) {
-    if (process.argv[j] !== biggest && i !== j) {
-      if (process.argv[j] > secondBiggest) {
-        secondBiggest = process.argv[j];
-      }
-    }
-  }
-  console.log(secondBiggest);
-} else {
-  console.log(0);
+// if (args.length > 1 && args !== firstArg && args !== secondArg) {
+//   for (let i = 0; i < args.length; i++) {
+//     if (biggest < process.argv[i]) {
+//       biggest = process.argv[i];
+//     }
+//   }
+//   let secondBiggest = process.argv[3];
+//   for (let j = 0; j < args.length; j++) {
+//     if (process.argv[j] !== biggest && i !== j) {
+//       if (process.argv[j] > secondBiggest) {
+//         secondBiggest = process.argv[j];
+//       }
+//     }
+//   }
+//   console.log(secondBiggest);
+// } else {
+//   console.log(0);
+// }
+
+let secondLargest = 0;
+const args = process.argv.slice(2);
+
+if (args.length > 1) {
+  args.sort();
+  secondLargest = args[args.length - 2];
 }
+console.log(secondLargest);
