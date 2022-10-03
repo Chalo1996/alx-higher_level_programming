d90aa8620a3fff605e8cbc96037308dd25337cab
Chalo1996 <emusyoka759@gmail.com>
2022-10-03 23:08:16 +0300

    int cmd arg

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const argNum = process.argv;
 if (isNaN(Number(argNum[2]))) {
   console.log('Not a number');
 } else {
-  console.log(`My number is ${argNum[2]}`);
+  console.log(`My number is: ${argNum[2]}`);
 }
