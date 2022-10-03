7494f44f69a0682d44036d57b83799cd11535333
Chalo1996 <emusyoka759@gmail.com>
2022-10-03 23:52:12 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const argNum = process.argv[2];
+
+if (isNaN(argNum)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < argNum; i++) {
+    let str = '';
+    for (let j = 0; j < argNum; j++) {
+      str += 'x';
+    }
+    console.log(str);
+  }
+}
