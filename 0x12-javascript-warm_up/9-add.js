2a1138f93c6683c3dd66028d08e46b32f0463f6f
Chalo1996 <emusyoka759@gmail.com>
2022-10-04 00:16:27 +0300

    sum

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const firstInt = process.argv[2];
+const secondInt = process.argv[3];
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return (NaN);
+  } else {
+    return (parseInt(a) + parseInt(b));
+  }
+}
+
+console.log(add(firstInt, secondInt));
