60fd296073d1b83ba0e83e63fb7c57b9ab9fee31
Chalo1996 <emusyoka759@gmail.com>
2022-10-03 23:27:26 +0300

    int arg

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-'use strict';
-
-let argNum = process.argv[2];
+const argNum = process.argv[2];
 
 if (isNaN(argNum)) {
   console.log('Not a number');
 } else {
-  console.log('My number is: '+argNum);
+  console.log('My number: ' + Math.floor(argNum));
 }
