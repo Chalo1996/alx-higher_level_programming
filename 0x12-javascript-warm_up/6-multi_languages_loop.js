1884f0e4413f08a8fedce46cc85517e7ca1778d5
Chalo1996 <emusyoka759@gmail.com>
2022-10-03 23:03:43 +0300

    "array looping"

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const argArr = ['C is fun', 'Python is amazing', 'JavaScript is amazing'];
+const argArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < argArr.length; i++) {
   console.log(argArr[i]);
