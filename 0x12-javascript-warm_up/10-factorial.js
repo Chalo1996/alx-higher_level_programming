7814f8fbb027f154144f5f3a3ccc9e3f1b699b66
Chalo1996 <emusyoka759@gmail.com>
2022-10-04 00:26:51 +0300

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const firstInt = process.argv[2];
+
+const fact = (x) => {
+  if (isNaN(x)) {
+    return (1);
+  } else {
+    if (x === 1) {
+      return (1);
+    } else {
+      return (x * fact(x - 1));
+    }
+  }
+};
+
+console.log(fact(firstInt));
