14d0919ec2284d239f2d7b565c63028130fdb051
Chalo1996 <emusyoka759@gmail.com>
2022-10-08 22:50:38 +0300

    pass val in argv

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""that takes in an argument and displays all\
+    values in the states table of hbtn_0e_0_usa\
+    where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+arg = argv[4]
+
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE "{:s}"\
+        ORDER BY id ASC'.format(arg))
+    vals = cur.fetchall()
+
+    for val in vals:
+        if val[1] == arg:
+            print(val)
+
+    cur.close()
+    conn.close()
