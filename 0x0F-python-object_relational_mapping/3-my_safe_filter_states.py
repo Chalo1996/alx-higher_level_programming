0aac8d9173c397a24dd8d5641999168312e0c0af
Chalo1996 <emusyoka759@gmail.com>
2022-10-08 23:15:59 +0300

    free from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" safe from MySQL injections!
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    arg = ''
+    for argName in argv[4:]:
+        arg += argName
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset='utf8'
+    )
+
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name LIKE "{:s}"\
+        ORDER BY id ASC'.format(arg))
+
+    argsNames = cur.fetchall()
+
+    for argsName in argsNames:
+        if argsName[1] in arg:
+            print(argsName)
+
+    cur.close()
+    conn.close()
