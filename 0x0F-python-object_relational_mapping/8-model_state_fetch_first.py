37d5b3df9ea97d3221b6e403a3e89823628761a1
Chalo1996 <emusyoka759@gmail.com>
2022-10-09 15:12:50 +0300

    first state.id

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,8 +23,11 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if state.id is not None and state.id == 1:
-            print("{}: {}".format(state.id, state.name))
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is not None and state.id == 1:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print('Nothing')
 
     session.close()
