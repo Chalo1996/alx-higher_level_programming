09ac9d0317caa1179f5c3bb00052fcc77a38d6ab
Chalo1996 <emusyoka759@gmail.com>
2022-10-09 16:00:54 +0300

    name as argv

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     state_name = session.query(State).filter_by(name=arg).first()
 
     if arg is not None:
-        print(state_name.id)
+        print(str(state_name.id))
     else:
         print("Not found")
 
