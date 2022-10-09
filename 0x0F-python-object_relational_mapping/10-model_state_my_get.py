e00e556d2b296430bb172c5394ef65777afa7a4e
Chalo1996 <emusyoka759@gmail.com>
2022-10-09 15:59:50 +0300

    name as argv

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""prints the State object with the name\
+    passed as argument from the database hbtn_0e_6_usa
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    arg = argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]
+        )
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name = session.query(State).filter_by(name=arg).first()
+
+    if arg is not None:
+        print(state_name.id)
+    else:
+        print("Not found")
+
+    session.close()
