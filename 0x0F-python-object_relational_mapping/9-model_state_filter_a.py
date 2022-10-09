932246fc057a5e597150f469e3a5d0bedb487e47
Chalo1996 <emusyoka759@gmail.com>
2022-10-09 15:31:21 +0300

    contains n in state.name

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""lists all State objects that contain the\
+    letter a from the database hbtn_0e_6_usa
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3]
+        )
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+
+    session.close()
