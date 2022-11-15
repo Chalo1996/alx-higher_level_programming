103a522fa3ae594c1ff63aaf3f6381c1356d5cca
Chalo1996 <emusyoka759@gmail.com>
2022-11-16 02:17:41 +0300

    displays the status code of a request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const myArgs = process.argv.slice(2);
 const req = require('request');
 
-req(myArgs[0], (err, req, res) => {
+req(myArgs[0], (err, res, req) => {
   if (err) throw err;
 
   const status = res && res.statusCode;
-  console.log(status);
+  console.log(`code: ${status}`);
 });
