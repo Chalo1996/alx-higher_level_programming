cf35280a2a6c1652da207eb2825188cdf0305107
Chalo1996 <emusyoka759@gmail.com>
2022-11-16 02:11:18 +0300

    get status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2);
+const req = require('request');
+
+req(myArgs[0], (err, req, res) => {
+  if (err) throw err;
+
+  const status = res && res.statusCode;
+  console.log(status);
+});
