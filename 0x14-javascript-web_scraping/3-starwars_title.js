f55b2a512fce839ddc88a4e5b1b0ec9a8083cb18
Chalo1996 <emusyoka759@gmail.com>
2022-11-16 02:42:01 +0300

    movie titles

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArgs = process.argv.slice(2);
+const res = require('request');
+url = 'https://swapi-api.hbtn.io/api/films/';
+
+res(url + myArgs[0], (err, res, body) => {
+  if (err) throw err;
+
+  console.log(JSON.parse(body).title);
+});
